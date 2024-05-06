daa4849e22704f6dfbf49eddf11d6d32f8f1ce1f
aksh sood <aksh.sood@batonsystems.com>
2024-05-06 17:39:13 +0530

    sqs encryption

--- commons/aws/sqs/main.tf
+++ commons/aws/sqs/main.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "this" {
   name                       = var.name
   visibility_timeout_seconds = 305
-  sqs_managed_sse_enabled    = false
+  sqs_managed_sse_enabled    = true
   tags                       = var.tags
 }
