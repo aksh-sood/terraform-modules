0b772c543a9b59c009994637072cdff1b040cc8a
aksh sood <aksh.sood@batonsystems.com>
2024-05-06 17:38:41 +0530

    block public access on S3

--- commons/aws/s3/main.tf
+++ commons/aws/s3/main.tf
@@ -1,6 +1,15 @@
-resource "aws_s3_bucket" "s3-bucket" {
+resource "aws_s3_bucket" "this" {
   bucket = var.name
   tags   = var.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3-bucket" {
